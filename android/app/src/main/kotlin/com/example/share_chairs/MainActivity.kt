75349824f06b46e52445ef984dd37513a922b7eb
Sabareeswaran B <sabareeswaran2510@gmail.com>
2021-10-31 13:11:14 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/share_chairs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.share_chairs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/repository/user_repository.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:api_cache_manager/models/cache_db_model.dart';
+import 'package:api_cache_manager/utils/cache_manager.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class UserRepository {
+
+  Future signin() async {
+    
+  }
+
+  Future signout() async {
+
+  }
+}
